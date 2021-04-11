aabc492ccf9f4330068f7e0aa36d207e455746e2
munya <oshgtyou@gmail.com>
2021-04-12 01:28:54 +0900

    ページ追加

--- tng_ec_subject/database/migrations/2014_10_12_000000_create_users_table.php
+++ tng_ec_subject/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -20,7 +20,13 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('tel_number');
+            $table->string('address');
+            $table->id('seller_id');
             $table->timestamps();
+
+
+
         });
     }
 
--- tng_ec_subject/routes/web.php
+++ tng_ec_subject/routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
